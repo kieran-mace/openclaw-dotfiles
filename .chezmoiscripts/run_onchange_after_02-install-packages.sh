14ad44dbc586264dab5b9698acbd6e53320bdbeb
Kieran Mace <kieran@plinth.co>
2026-03-20 11:44:58 -0500

    Remove invalid --no-lock flag from brew bundle

--- .chezmoiscripts/run_onchange_after_02-install-packages.sh
+++ .chezmoiscripts/run_onchange_after_02-install-packages.sh
@@ -6,4 +6,4 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # Install all packages from Brewfile
 # chezmoi hash: {{ include "Brewfile" | sha256sum }}
 echo "Installing Brewfile packages..."
-brew bundle --file="{{ .chezmoi.sourceDir }}/Brewfile" --no-lock
+brew bundle --file="{{ .chezmoi.sourceDir }}/Brewfile"
